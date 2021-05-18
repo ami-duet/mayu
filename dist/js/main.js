a9ed5ffe8a294f9bfc313f17c159563c5c3151f8
Anne-Marie Dufour <hello@annemariedufour.com>
2021-05-18 19:01:37 +0200

    Add carousel script, initialize carousel and add villages

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,12 @@
 /*! project-name v0.0.1 | (c) 2021 YOUR NAME | MIT License | http://link-to-your-git-repo.com */
 // Append villages illustrations and descriptions
-const villages = d3.select('.villages')
-  .append('div')
-    .attr('class', 'container')
-  .selectAll('.village')
+const villages = d3.select('.glide__slides')
+  .selectAll('.glide__slide')
   .data(villagesData)
   .join('div')
-    .attr('class', d => `village village-${d.village_id}`)
+    .attr('class', 'glide__slide')
   .append('div')
-    .attr('class', 'village-container');
+    .attr('class', d => `village village-${d.village_id}`);
 villages
   .append('h2')
     .text(d => d.village_name);
@@ -28,3 +26,11 @@ section
   .append('div')
     .attr('class', 'sct-description')
   .html(d => d.description);
+
+// Initialize carousel
+new Glide('.glide', {
+  type: 'carousel',
+  focusAt: 'center',
+  perView: 3,
+  startAt: 4
+}).mount();
